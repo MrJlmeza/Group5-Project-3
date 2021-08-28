0d30019023136a5f5090b66fc6d0128edfb9e02c
tanmayrp <tanmayrp@gmail.com>
2021-08-28 14:32:10 -0400

    remove unnecessary files. Update path to include new flask routes. Angela's html stuff.

--- eagles_ml/static/js/app.js
+++ eagles_ml/static/js/app.js
@@ -1,40 +1,77 @@
+function GetTessaractOCR()
+{
+  Tesseract.recognize(
+    '../assets/'+ photo + '.JPG',
+    'eng',
+    { logger: m => console.log(m) }
+      ).then(({ data: { text } }) => {
+    console.log(text);
+    })
+}
 
-function BrandonGraham1Clicked() {
+
+function BrandonGraham1_clicked() {
   const url = "/api/eagles_ml/BrandonGraham1"
   d3.json(url).then(function(response) {
     console.log(response)
 
   });
-
-  // Tesseract.recognize(
-  //   'http://127.0.0.1:5000/static/assets/BrandonGraham1.JPG',
-  //   'eng',
-  //   { logger: m => console.log(m) }
-  //     ).then(({ data: { text } }) => {
-  //   console.log(text);
-  //   })
-
+  // photo = 'BrandonGraham1'
+  // GetTessaractOCR(photo);
 
 }
 
-function BrandonGraham_55_skewedClicked() {
+function BrandonGraham_55_skewed_clicked() {
   const url = "/api/eagles_ml/BrandonGraham_55_skewed"
   d3.json(url).then(function(response) {
     console.log(response)
 
   });
+  // photo = 'BrandonGraham_55_skewed'
+  // GetTessaractOCR(photo);
 }
 
-function MilesSanders_numberClear2Clicked() {
+function MilesSanders_numberClear2_clicked() {
   const url = "/api/eagles_ml/MilesSanders_numberClear2"
   d3.json(url).then(function(response) {
     console.log(response)
   });
+  // photo = 'MilesSanders_numberClear2'
+  // GetTessaractOCR(photo);
 }
 
-function MilesSanders_multiPlayersClicked(){
+function MilesSanders_numberClear_clicked() {
+  const url = "/api/eagles_ml/MilesSanders_numberClear"
+  d3.json(url).then(function(response) {
+    console.log(response)
+  });
+  // photo = 'MilesSanders_numberClear2'
+  // GetTessaractOCR(photo);
+}
+
+function MilesSanders_multiPlayers_clicked(){
   const url = "/api/eagles_ml/MilesSanders_multiPlayers"
   d3.json(url).then(function(response) {
     console.log(response)
   });
+  // photo = 'MilesSanders_multiPlayers'
+  // GetTessaractOCR(photo);
+}
+
+function BrianDawkins_helmetOff2_clicked(){
+  const url = "/api/eagles_ml/BrianDawkins_helmetOff2"
+  d3.json(url).then(function(response) {
+    console.log(response)
+  });
+  // photo = 'MilesSanders_multiPlayers'
+  // GetTessaractOCR(photo);
+}
+
+function BrianDawkins_helmetOn_clicked(){
+  const url = "/api/eagles_ml/BrianDawkins_helmetOn"
+  d3.json(url).then(function(response) {
+    console.log(response)
+  });
+  // photo = 'MilesSanders_multiPlayers'
+  // GetTessaractOCR(photo);
 }
